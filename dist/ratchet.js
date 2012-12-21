dbd49430824aa034be660e9ebf466c35cfc61c1e
Jacob Kelley <jakie8@gmail.com>
2012-12-21 12:38:52 -0800

    Added backpane.js to dist file

--- dist/ratchet.js
+++ dist/ratchet.js
@@ -69,6 +69,68 @@
   window.addEventListener('click', function (e) { if (getPopover(e)) e.preventDefault(); });
 
 }();
+
+/* ----------------------------------
+ * BACKPANE v1.0.0
+ * Licensed under The MIT License
+ * http://opensource.org/licenses/MIT
+ * ---------------------------------- */
+
+!function () {
+
+  var backPane;
+  
+  var findPanes = function (target) {
+    var i, panes = document.querySelectorAll('a');
+    for (; target && target !== document; target = target.parentNode) {
+      for (i = panes.length; i--;) { if (panes[i] === target) return target; }
+    }
+  };
+
+  var getPane = function (e) {
+    var anchor = findPanes(e.target);
+
+    if (!anchor || !anchor.hash) return;
+
+    backPane = document.querySelector(anchor.hash);
+
+    if (!backPane || !backPane.classList.contains('backPane')) return;
+
+    return backPane;
+  }
+
+  window.addEventListener('touchend', function (e) {
+    var backPane = getPane(e),
+      paneSide = e.srcElement.dataset.pane ? e.srcElement.dataset.pane : 'left',
+    	body = document.body,
+    	content = document.querySelector('.content');
+	
+    if (!backPane) return;
+	
+	if(paneSide=="left"){
+		if(body.classList.contains('openPaneLeft')){
+			body.classList.remove('openPaneLeft');
+		} else {
+			body.classList.add('openPaneLeft');
+		}
+	} else {
+		if(body.classList.contains('openPaneRight')){
+			content.addEventListener('webkitTransitionEnd', function(){
+				backPane.classList.remove('marginLeft');
+			});
+			body.classList.remove('openPaneRight');
+		} else {
+			backPane.classList.add('marginLeft');
+			body.classList.add('openPaneRight');
+		}
+	}
+	
+  });
+
+  window.addEventListener('click', function (e) { if (getPane(e)) e.preventDefault(); });
+
+}();
+
 /* ----------------------------------
  * PUSH v1.0.0
  * Licensed under The MIT License
