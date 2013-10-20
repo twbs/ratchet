dcafb2a209e1845a97ed8b1fda28219a1374066f
connors <connor.sears@github.com>
2013-10-19 21:34:39 -0700

    Adding initial pass at alert js.

--- dist/ratchet.js
+++ dist/ratchet.js
@@ -15,8 +15,7 @@
 
 !function () {
   var findModals = function (target) {
-    var i;
-    var modals = document.querySelectorAll('a');
+    var i, modals = document.querySelectorAll('a');
     for (; target && target !== document; target = target.parentNode) {
       for (i = modals.length; i--;) { if (modals[i] === target) return target; }
     }
@@ -776,4 +775,41 @@
     toggle    = false;
   });
 
+}();
+/* ----------------------------------
+ * ALERT v0.0.1
+ * Licensed under The MIT License
+ * http://opensource.org/licenses/MIT
+ * ---------------------------------- */
+
+!function () {
+  var findAlerts = function (target) {
+    var i, alerts = document.querySelectorAll('a');
+    for (; target && target !== document; target = target.parentNode) {
+      for (i = alerts.length; i--;) { if (alerts[i] === target) return target; }
+    }
+  };
+
+  var onAlertHidden = function () {
+    var alert = getAlert(event);
+
+    alert.style.display = 'none';
+    alert.removeEventListener('webkitTransitionEnd', onAlertHidden);
+  }
+
+  var getAlert = function (event) {
+    var alertDismiss = findAlerts(event.target);
+    if (alertDismiss && alertDismiss.hash) return document.querySelector(alertDismiss.hash);
+    console.log('get alert');
+  };
+
+  window.addEventListener('click', function (event) {
+    var alert = getAlert(event);
+    
+    if (alert) {
+      if (alert && alert.classList.contains('alert')) alert.classList.add('dismiss');
+      alert.addEventListener('webkitTransitionEnd', onAlertHidden);
+      event.preventDefault();
+    }
+  });
 }();
