b3860d19323df7c6de659e5231b85d4a09fa7c08
Chris Rebert <code@rebertia.com>
2014-02-25 18:40:53 -0800

    import change-version-number Grunt task from Bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,6 +138,17 @@ module.exports = function(grunt) {
       files: {
         src: '_site/**/*.html'
       }
+    },
+
+    sed: {
+      versionNumber: {
+        pattern: (function () {
+          var old = grunt.option('oldver');
+          return old ? RegExp.quote(old) : old;
+        })(),
+        replacement: grunt.option('newver'),
+        recursive: true
+      }
     }
   });
 
@@ -152,4 +163,9 @@ module.exports = function(grunt) {
   grunt.registerTask('validate-html', ['jekyll', 'validation']);
   grunt.registerTask('default', ['dist']);
   grunt.registerTask('build', ['dist']);
+
+  // Version numbering task.
+  // grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
+  // This can be overzealous, so its changes should always be manually reviewed!
+  grunt.registerTask('change-version-number', 'sed');
 };
