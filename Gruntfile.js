28d524c937f0fc2b152fd674689337909cc03d36
Chris Rebert <code@rebertia.com>
2014-02-25 16:18:43 -0800

    use /*! for attribution banner so it survives minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
       docsAssetsPath: 'docs/assets/'
     },
     
-    banner: '/*\n' +
+    banner: '/*!\n' +
             ' * =====================================================\n' +
             ' * Ratchet v<%= pkg.version %>\n' +
             ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
