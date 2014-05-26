69dbd3912719e798b709a2e0e62dd1b9f7669927
huang.xinghui <huang.x.hui@gmail.com>
2014-05-26 23:31:08 +0800

    modalToggle.hash add check contains slash

--- js/modals.js
+++ js/modals.js
@@ -24,7 +24,7 @@
 
   var getModal = function (event) {
     var modalToggle = findModals(event.target);
-    if (modalToggle && modalToggle.hash) {
+    if (modalToggle && modalToggle.hash && modalToggle.hash.indexOf('/') < 0) {
       return document.querySelector(modalToggle.hash);
     }
   };
