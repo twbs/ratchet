cb56ff2319b97253894cdb1358053728e69d9c70
XhmikosR <xhmikosr@gmail.com>
2015-01-30 11:15:31 +0200

    Run `grunt`.

--- docs/dist/css/ratchet-theme-ios.css
+++ docs/dist/css/ratchet-theme-ios.css
@@ -1,7 +1,7 @@
 /*!
  * =====================================================
  * Ratchet v2.0.2 (http://goratchet.com)
- * Copyright 2014 Connor Sears
+ * Copyright 2015 Connor Sears
  * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
  *
  * v2.0.2 designed by @connors.
